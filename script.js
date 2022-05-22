840d15366bbee6e5dc370f319c88940384272681
HPTR <76168685+HPTR@users.noreply.github.com>
2022-05-22 14:25:44 +0100

    Simplify some statements and add descriptive comments

--- script.js
+++ script.js
@@ -43,23 +43,22 @@ const handleNumberPress = (event) => {
     };
 
     //If a button was last pressed, add the pressed number to the current displayed one, if not, start a new number
-    if (calculator['lastPressedType'] === 'number') {
-        display.value += event.target.value;
-     } else {
-        display.value = event.target.value;
-    };
+    calculator['lastPressedType'] === 'number' ? display.value += event.target.value : display.value = event.target.value;
 
     calculator.lastPressedType = 'number';
 }
 
 const handleOperatorPress = (event) => {
 
+    //Remove the pressed modifier from other operators
     allOperators.forEach((button) => {
             button.classList.remove('calculator__button--pressed');
     });
 
+    //Add the pressed modifier to the pressed operator
     event.target.classList.add('calculator__button--pressed');
     
+    //Save pressed operator to the calculator object
     switch (event.target.value) {
         case 'add':
             calculator.operator = '+';
@@ -75,6 +74,7 @@ const handleOperatorPress = (event) => {
             break;
     };
 
+    //Add the currently displayed number to the calculator object
     calculator.numOne = Number(display.value);
 
     calculator.lastPressedType = 'operator';
@@ -82,33 +82,35 @@ const handleOperatorPress = (event) => {
 
 const handleCalculatePress = (event) => {
 
-    if (calculator['lastPressedType'] === 'calculate') {
-        calculator.numOne = display.value;
-    } else {
-        calculator.numTwo = Number(display.value);
-    };
+    //If the last pressed button was calculate, use the calculated result to perform the calculation again on that value
+    calculator['lastPressedType'] === 'calculate' ? calculator.numOne = display.value : calculator.numTwo = Number(display.value)
 
+
+    //If calculate is pressed and a number is missing, do nothing
     if (!calculator.numTwo || !calculator.numOne) {
         display.value = display.value
         calculator.lastPressedType = 'calculate';
         return;
     }
 
+
+    //Perform the calculation and save the result to the calculator object
     switch (calculator.operator) {
         case '+':
             calculator.result = Number(calculator.numOne) + Number(calculator.numTwo);
             break;
         case '-':
-            calculator.result = calculator.numOne - calculator.numTwo;
+            calculator.result = Number(calculator.numOne) - Number(calculator.numTwo);
             break;
         case '*':
-            calculator.result = calculator.numOne * calculator.numTwo;
+            calculator.result = Number(calculator.numOne) * Number(calculator.numTwo);
             break;
         case '/':
-            calculator.result = calculator.numOne / calculator.numTwo;
+            calculator.result = Number(calculator.numOne) / Number(calculator.numTwo);
             break;
     };
 
+    //Display the result at a maximum of 8 numbers
     display.value = parseFloat(calculator.result.toFixed(8));
 
     calculator.lastPressedType = 'calculate';
@@ -116,33 +118,37 @@ const handleCalculatePress = (event) => {
 
 const handleFunctionPress = (event) => {
 
+
+    //If clear was pressed, and the last pressed button was also clear, remove saved numbers, result, and operator from the calculator object. If clear was not the last pressed button, remove the currently displayed value and change the text on the button
     if (event.target.value === 'clear') {
 
-    if (calculator['lastPressedType'] === 'clear') {
-        calculator.numOne = undefined;
-        calculator.numTwo = undefined;
-        calculator.operator = undefined;
-        calculator.result = undefined;
-
-        allOperators.forEach((button) => {
+        if (calculator['lastPressedType'] === 'clear') {
+            calculator.numOne = undefined;
+            calculator.numTwo = undefined;
+            calculator.operator = undefined;
+            calculator.result = undefined;
+    
+            allOperators.forEach((button) => {
                 button.classList.remove('calculator__button--pressed');
-        });
-
-    } else {
-        display.value = '0';
-        clear.innerText = 'AC';
-    };
-
-    calculator.lastPressedType = 'clear';
+            });
+    
+        } else {
+            display.value = '0';
+            clear.innerText = 'AC';
+        };
+    
+        calculator.lastPressedType = 'clear';
 
     }
 
+    //If percentage is pressed, divide the number by 100 - to be used to calculate the percentage of another number
     if (event.target.value === 'percentage') {
         display.value = Number(display.value) / 100;
 
         calculator.lastPressedType = 'percentage';
     }
 
+    //If polarity is pressed, add a minus to the front of the displayed number, if one is already present, remove it
     if (event.target.value === 'polarity') {
         if (display.value[0] === '-') {
             display.value = display.value.substring(1);
@@ -154,10 +160,6 @@ const handleFunctionPress = (event) => {
     }
 
 
-}
-
-const handlePercentagePress = (event) => {
-    display.value = display.value / 100;
 }
 
 // Event Listeners
